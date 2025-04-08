086bdf7ef963d7e9550b77db4527a1fa04d020b5
amalm2002 <amalm2495@gmail.com>
2025-04-08 19:14:09 +0530

    first auth-service commit

---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import path from "path";
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
+import 'dotenv/config'
+
+import { AuthController } from "./controller/authController";
+
+const authController=new AuthController()
+
+const packageDef=protoLoader.loadSync(path.resolve(__dirname,'./proto/auth.proto'),{
+    keepCase:true,
+    longs:String,
+    enums:String,
+    defaults:true,
+    oneofs:true
+})
+
+
+
+const grpcObject = grpc.loadPackageDefinition(packageDef) as unknown as any;
+const authpackage = grpcObject.authpackage;
+
+if(!authpackage || !authpackage.Auth || !authpackage.Auth.service){
+  console.error("Failed to load the User service from the proto file.");
+  process.exit(1);
+}
+
+
+const server = new grpc.Server();
+
+server.addService(authpackage.Auth.service,{
+  IsAuthenticated:authController.isAuthenticated,
+  RefreshToken:authController.verifyToken
+});
+
+const grpcServer = () =>{
+  const port = process.env.PORT;
+  const Domain = process.env.NODE_ENV ==="dev" ? process.env.DEV_DOMAIN : process.env.PRO_DOMAIN_USER;
+
+  server.bindAsync(`${Domain}:${port}`,grpc.ServerCredentials.createInsecure(),(err,bindPort)=>{
+    if(err){
+      console.error("Error starting gRPC server:", err);
+     return      
+    }
+
+    console.log(`gRPC auth server started on port:${bindPort}`);
+    
+  })
+}
+
+grpcServer();
